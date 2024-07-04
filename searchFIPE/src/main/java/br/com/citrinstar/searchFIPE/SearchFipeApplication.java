6f2138b6d7a14deccba3a8023ad2fcbfb646bf45
Citrin <lima.stephanielemos@gmail.com>
2024-07-04 19:24:25 -0300

    Starting the project with spring

---
+++ searchFIPE/src/main/java/br/com/citrinstar/searchFIPE/SearchFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.citrinstar.searchFIPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchFipeApplication.class, args);
+	}
+
+}
